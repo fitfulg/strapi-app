599dd2a7235ee6061075413a6cece0360264a0a2
ercardona <ercardona@edrans.com>
2021-08-06 21:35:45 +0200

    back and front

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9a03f970a5d3152355dde8278192fc42'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1f085b0b-545c-474d-a85d-6548c5cde4ef'
+};
